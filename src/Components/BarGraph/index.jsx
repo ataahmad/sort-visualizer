ad3e95d4df1992a248545ab149e359fb3407e89f
Ata Ahmad <ataahmad@Atas-MBP.lan>
2022-12-30 00:50:18 -0800

    adds in testing to check validity of insertion sort method

--- src/Components/BarGraph/index.jsx
+++ src/Components/BarGraph/index.jsx
@@ -41,27 +41,55 @@ const BarGraph = () => {
     }, [executed]);
 
     // Function to sort an array using insertion sort
-    const insertionSort = () => {
-        let arr = array;
-        let n = array.length;
+    const insertionSort = (arr, n) => {
+        const res = arr;
         let i, key, j;
         for (i = 1; i < n; i++) {
-            key = arr[i];
-            changeSelected(key);
+            key = res[i];
             j = i - 1;
 
             /* Move elements of arr[0..i-1], that are 
             greater than key, to one position ahead 
             of their current position */
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
+            while (j >= 0 && res[j] > key) {
+                res[j + 1] = res[j];
                 j = j - 1;
-                let newArr = [...arr];
-                console.log(newArr);
-                setArray(newArr);
             }
-            arr[j + 1] = key;
+            res[j + 1] = key;
         }
+        return res;
+    }
+
+
+    const generateArray = () => {
+        return Array.from({length: 100}, () => Math.floor(Math.random() * 100));
+    }
+
+    const testSortMethod = () => {
+        const testSubject = generateArray();
+        if (compareArrays(testSubject.sort(), insertionSort(testSubject))){
+            console.log("Test Passed! Arrays are equal");
+            return true;
+        }
+        console.log("Test Failed");
+        return false;
+    }
+
+    
+
+    const compareArrays = (arr1, arr2) => {
+        if (arr1.length != arr2.length) {
+            console.log("Test Failed");
+            return false;
+        }
+        for (let i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                console.log("Test Failed");
+                return false;
+            }
+        }
+
+        return true;
     }
 
 
@@ -81,8 +109,9 @@ const BarGraph = () => {
                 Switch
             </button>
             <button onClick={() => {
-                let newExecution = !executed;
-                execution(newExecution);
+                // console.log(generateArray());
+                testSortMethod();
+                // insertionSort()
             }}>
                 Insertion Sort
             </button>
