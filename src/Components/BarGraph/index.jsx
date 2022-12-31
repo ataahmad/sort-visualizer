582f26ef926df6e8b61fc30c95ab27936d19c589
Ata Ahmad <ataahmad@Atas-MBP.lan>
2022-12-31 13:22:25 -0800

    Updates the barGraph component to re-render the UI on each step of the sort

--- src/Components/BarGraph/index.jsx
+++ src/Components/BarGraph/index.jsx
@@ -1,27 +1,8 @@
 import React from "react";
 import './BarGraph.css'
 
-
-// Function to sort an array using insertion sort
-async function insertionSort(arr, n) {
-    let i, key, j;
-    for (i = 1; i < n; i++) {
-        key = arr[i];
-        j = i - 1;
-
-        /* Move elements of arr[0..i-1], that are 
-        greater than key, to one position ahead 
-        of their current position */
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-        arr[j + 1] = key;
-    }
-}
-
-const generateNewArray = () => {return Array.from({ length: 20 }, () => Math.floor(Math.random() * 100))};
-
+const generateNewArray = () => { return Array.from({ length: 20 }, () => Math.floor(Math.random() * 100)) };
+const TIMEOUT_INT = 500;
 
 class Bar extends React.Component {
     constructor(props) {
@@ -65,11 +46,41 @@ class BarGraph extends React.Component {
         this.setState({ arr: newData });
     }
 
+    sort = () => {
+        let arr = [...this.state.arr];
+        let n = arr.length;
+        let i = 1;
+        let key, j;
+    
+        const sortStep = () => {
+          if (i < n) {
+            key = arr[i];
+            j = i - 1;
+    
+            /* Move elements of arr[0..i-1], that are 
+            greater than key, to one position ahead 
+            of their current position */
+            while (j >= 0 && arr[j] > key) {
+              arr[j + 1] = arr[j];
+              j = j - 1;
+            }
+            arr[j + 1] = key;
+            i++;
+    
+            // Update the state and schedule the next step
+            this.setState({ arr: [...arr] }, () => setTimeout(sortStep, TIMEOUT_INT));
+          }
+        };
+    
+        // Schedule the first step
+        setTimeout(sortStep, TIMEOUT_INT);
+      };
+
     render() {
         return (
             <div>
                 <button onClick={this.refreshArray}> Refresh </button>
-                <button>
+                <button onClick={this.sort}>
                     Insertion Sort
                 </button>
                 <div className="barFrame">
