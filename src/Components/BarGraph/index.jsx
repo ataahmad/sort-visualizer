6709e8597deb7d73210bfaee38dbd7bbf107a5ac
Ata Ahmad <ataahmad@Atas-MBP.lan>
2022-12-30 19:47:03 -0800

    Reworks bar graph to be written as a class for easier handling of component lifecycle.

--- src/Components/BarGraph/index.jsx
+++ src/Components/BarGraph/index.jsx
@@ -1,137 +1,87 @@
-import { hasSelectionSupport } from "@testing-library/user-event/dist/utils";
-import React, { useState } from "react";
-import { useEffect } from "react";
+import React from "react";
 import './BarGraph.css'
-/*
-Bar Graph component:
-props
-    - Should have an array property which holds all values for bars
-    - Should take in an array length to indicate how many bars we are working with.
-    - Should have a method that resets the array to a new one
-
-Create a function called switch, and a state called switch that has a value of an
-integer from 0 too n.
-Create a button that on click, changes the value of the switch state to a new random
-number between 0 and n.
-Create a useEffect function that targets the 0th element and the ith element (i being
-    the new random number that was generated and changes the state of the switch state)
-    and switches them.
-*/
 
 
-const BarGraph = () => {
+// Function to sort an array using insertion sort
+async function insertionSort(arr, n) {
+    let i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
 
-    const [array, setArray] = useState([]);
-    const [ARR_LENTGH, setLength] = useState(10);
-    const [WINDOW_HEIGHT, setHeight] = useState(100);
-    const [refresh, refreshArray] = useState(true);
-    const [selected, changeSelected] = useState(0);
-    const [executed, execution] = useState(false);
-
-    useEffect(() => {
-        const newArray = [];
-        for (let i = 0; i < ARR_LENTGH; i++) {
-            newArray.push(Math.floor(Math.random() * WINDOW_HEIGHT) + 1)
+        /* Move elements of arr[0..i-1], that are 
+        greater than key, to one position ahead 
+        of their current position */
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
         }
-        setArray(newArray);
-    }, [refresh]);
-
-    useEffect(() => {
-        insertionSort();
-    }, [executed]);
-
-    // Function to sort an array using insertion sort
-    const insertionSort = (arr, n) => {
-        const res = arr;
-        let i, key, j;
-        for (i = 1; i < n; i++) {
-            key = res[i];
-            j = i - 1;
-
-            /* Move elements of arr[0..i-1], that are 
-            greater than key, to one position ahead 
-            of their current position */
-            while (j >= 0 && res[j] > key) {
-                res[j + 1] = res[j];
-                j = j - 1;
-            }
-            res[j + 1] = key;
-        }
-        return res;
+        arr[j + 1] = key;
     }
-
-
-    const generateArray = () => {
-        return Array.from({length: 100}, () => Math.floor(Math.random() * 100));
-    }
-
-    const testSortMethod = () => {
-        const testSubject = generateArray();
-        if (compareArrays(testSubject.sort(), insertionSort(testSubject))){
-            console.log("Test Passed! Arrays are equal");
-            return true;
-        }
-        console.log("Test Failed");
-        return false;
-    }
-
-    
-
-    const compareArrays = (arr1, arr2) => {
-        if (arr1.length != arr2.length) {
-            console.log("Test Failed");
-            return false;
-        }
-        for (let i = 0; i < arr1.length; i++) {
-            if (arr1[i] != arr2[i]) {
-                console.log("Test Failed");
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-
-    return (
-        <div>
-            <button onClick={() => {
-                let newRefresh = !refresh;
-                refreshArray(newRefresh);
-            }}> Refresh </button>
-            <button onClick={() => {
-                let newArr = [...array];
-                let temp = newArr[0];
-                newArr[0] = newArr[3];
-                newArr[3] = temp;
-                setArray(newArr);
-            }}>
-                Switch
-            </button>
-            <button onClick={() => {
-                // console.log(generateArray());
-                testSortMethod();
-                // insertionSort()
-            }}>
-                Insertion Sort
-            </button>
-            <div className="barFrame">
-                {
-                    array.map((val, ind) => {
-                        return (<div
-                            className={"bar" + (ind == selected ? " selected" : "")}
-                            refer={String(ind)}
-                            style={{ height: String(val * 5) + 'px' }}
-                        >
-                            {val}
-                        </div>)
-                    })
-                }
-            </div>
-        </div>
-    )
 }
 
+const generateNewArray = () => {return Array.from({ length: 20 }, () => Math.floor(Math.random() * 100))};
 
+
+class Bar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: this.props.data
+        };
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            data: nextProps.data
+        });
+    }
+
+    render() {
+        return (<div
+            className="bar"
+            value={String(this.state.data)}
+            style={{ height: String(this.state.data * 5) + 'px' }}
+        >
+        </div>)
+    }
+}
+
+class BarGraph extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            arr: []
+        }
+    }
+
+    componentDidMount() {
+        const randomData = generateNewArray();
+        this.setState({ arr: randomData });
+    }
+
+    refreshArray = () => {
+        const newData = generateNewArray();
+        this.setState({ arr: newData });
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.refreshArray}> Refresh </button>
+                <button>
+                    Insertion Sort
+                </button>
+                <div className="barFrame">
+                    {
+                        this.state.arr.map(item => {
+                            return <Bar data={item} />
+                        })
+                    }
+                </div>
+            </div>
+        )
+    }
+}
 
 export default BarGraph;
