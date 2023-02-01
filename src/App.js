fad7ec4ac803f2a9e68cec641c2c120d457f31cf
Ata Ahmad <ataahmad@Atas-MBP.lan>
2023-02-01 14:58:32 -0800

    Fixes lint issue

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         Sorting Visualizer <small>v{pjson.version}</small>
       </header>
-      <BarGraph/>
+      <BarGraph />
     </div>
   );
 }
