04d676144ae6044127d0be152e314296aefb7433
Ata Ahmad <ataahmad@Atas-MBP.lan>
2023-02-01 15:45:56 -0800

    Removed redundnt button roles

--- src/Components/BarGraph/index.jsx
+++ src/Components/BarGraph/index.jsx
@@ -75,15 +75,15 @@ class BarGraph extends React.Component {
             <div>
                 <div className="buttonBar">
                     <div className="configButtons">
-                        <button className="button-27" role="button" onClick={this.refreshArray}> Refresh </button>
+                        <button className="button-27" onClick={this.refreshArray}> Refresh </button>
                     </div>
                     <div className="algoButtons">
-                        <button className="button-27" role="button" onClick={this.insertionSort}> Insertion Sort </button>
+                        <button className="button-27" onClick={this.insertionSort}> Insertion Sort </button>
                     </div>
                 </div>
                 <div className="barFrame">
                     <div className="algoButtons">
-                        <button className="button-27 sort" role="button" onClick={this.insertionSort}> Sort </button>
+                        <button className="button-27 sort" onClick={this.insertionSort}> Sort </button>
                     </div>
                     {
                         this.state.arr.map((item, index) => {
